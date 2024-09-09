704405a28043d5ec2e34f7f7c08738bbc5b1108c
GouthamNP <107291366+GouthamOfTheNP@users.noreply.github.com>
2024-09-08 21:52:44 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        for (var i = 0; i < 5; i++) {
+            for (var c = 0; i < 5; i++) {
+                for (var y = 0; i < 5; i++) {
+                    for (var e = 0; i < 5; i++) {
+                        for (var d = 0; i < 5; i++) {
+                            for (var k = 0; i < 5; i++) {
+                                for (var l = 0; l < 5; l++) {
+                                    for (var j = 0; l < 5; l++) {
+                                        for (var o = 0; l < 5; l++) {
+                                            System.out.println("E");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
